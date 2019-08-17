4d62b56b8a77f55fd2c2e28fd1679067a19232ee
Dinesh <dineshsastha@github.com>
2019-08-18 00:35:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import { Registration } from './Registration';
+
+class App extends React.Component {
+
+  render(){  
+return (
+
+<div>
+  <Registration />
+  </div>
+
+  );
+}
+
+
+}
+
+export default App;
